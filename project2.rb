9da1ff84f5f3b90e0f069801827ccc1c1b5efb91
spencer burke <info.spencer.burke@gmail.com>
2026-04-23 18:46:16 -0700

    Add a version of the FIFO

--- project2.rb
+++ project2.rb
@@ -22,29 +22,22 @@ end
 
 PAGE_FRAME_SIZES = [3, 4, 5, 6].freeze
 
-=begin
-conducting a performance analysis of them based on the performance measure of page faults for each page replacement algorithm using multiple inputs.
-Output the details of each algorithm’s execution.
-You need to show what pages are inside the page frames along with the reference string and mark it when a page fault occurred.
-You can choose your display format, for examples, you can display the results for each reference string in a table format as shown in the class notes.
-=end
-
 # First In First Out(FIFO)
 def fifo(page_frame_size, reference_line, output = false)
   faults = 0
   page_table = Array.new(page_frame_size)
   current_table_slot = 0
 
-  # print out reference string if logging enabled
   if output
     puts("FIFO algorithm with reference string: #{reference_line}")
   end
 
-  # start processing
-  # convert reference string into array of pages
   pages = reference_line.split("").map(&:to_i)
 
-  # iterate through each page
+  if output
+    puts("Page table start: #{page_table}")
+  end
+  
   pages.each do |new_page|
     curr_page = page_table[current_table_slot % page_frame_size]
 
@@ -55,6 +48,10 @@ def fifo(page_frame_size, reference_line, output = false)
     page_table[current_table_slot % page_frame_size] = new_page
 
     current_table_slot += 1
+
+    if output
+      puts("Page table: #{page_table} new page: #{new_page} faults: #{faults}")
+    end
   end
 
   return faults
