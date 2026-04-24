cf937986555bf9e0393e134b203d5418c0779256
spencer burke <info.spencer.burke@gmail.com>
2026-04-23 19:26:11 -0700

    Add the working lru implementation

--- project2.rb
+++ project2.rb
@@ -39,18 +39,19 @@ def fifo(page_frame_size, reference_line, output = false)
   end
   
   pages.each do |new_page|
-    curr_page = page_table[current_table_slot % page_frame_size]
+    if page_table.include?(new_page)
+      if output
+        puts("Page table: #{page_table} new page: #{new_page} faults: #{faults} HIT")
+      end
+    else
+      page_table[current_table_slot % page_frame_size] = new_page
 
-    if curr_page != new_page
+      current_table_slot += 1
       faults += 1
-    end
 
-    page_table[current_table_slot % page_frame_size] = new_page
-
-    current_table_slot += 1
-
-    if output
-      puts("Page table: #{page_table} new page: #{new_page} faults: #{faults}")
+      if output
+        puts("Page table: #{page_table} new page: #{new_page} faults: #{faults} MISS")
+      end
     end
   end
 
@@ -58,11 +59,52 @@ def fifo(page_frame_size, reference_line, output = false)
 end
 
 # Least Recently Used (LRU)
-def lru(page_frame_size, line)
+def lru(page_frame_size, reference_line, output = false)
+  faults = 0
+  # start with an empty array to make sure length works properly
+  page_stack = []
+  current_table_slot = 0
+
+  if output
+    puts("LRU algorithm with reference string: #{reference_line}")
+  end
+
+  pages = reference_line.split("").map(&:to_i)
+
+  if output
+    puts("Page table start: #{page_stack}")
+  end
+
+  pages.each do |new_page|
+    if page_stack.include?(new_page)
+      page_stack.delete(new_page)
+      page_stack.push(new_page)
+
+      if output
+        puts("Page table: #{page_stack} new page: #{new_page} faults: #{faults} HIT")
+      end
+    else
+      if page_stack.length < page_frame_size
+        page_stack.push(new_page)
+      else
+        page_stack.shift
+        page_stack.push(new_page)
+      end
+      faults += 1
+
+      if output
+        puts("Page table: #{page_stack} new page: #{new_page} faults: #{faults} MISS")
+      end
+    end
+  end
+
+  return faults
 end
 
 # Optimal Algorithm (OPT)
-def opt(page_frame_size, line)
+def opt(page_frame_size, reference_line, output = false)
 end
 
-fifo(3, "1234567", true)
+fifo(3, "70120304230321201701", true)
+lru(3, "70120304230321201701", true)
+opt(3, "70120304230321201701", true)
