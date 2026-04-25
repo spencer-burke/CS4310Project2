65ee5aba03dfd4a38610402f2d1dd87de3419744
spencer burke <info.spencer.burke@gmail.com>
2026-04-24 17:33:13 -0700

    Add the working opt implementation

--- project2.rb
+++ project2.rb
@@ -63,7 +63,6 @@ def lru(page_frame_size, reference_line, output = false)
   faults = 0
   # start with an empty array to make sure length works properly
   page_stack = []
-  current_table_slot = 0
 
   if output
     puts("LRU algorithm with reference string: #{reference_line}")
@@ -90,6 +89,7 @@ def lru(page_frame_size, reference_line, output = false)
         page_stack.shift
         page_stack.push(new_page)
       end
+
       faults += 1
 
       if output
@@ -103,6 +103,64 @@ end
 
 # Optimal Algorithm (OPT)
 def opt(page_frame_size, reference_line, output = false)
+  faults = 0
+  page_table = []
+
+  if output
+    puts("OPT algorithm with reference string: #{reference_line}")
+  end
+
+  pages = reference_line.split("").map(&:to_i)
+
+  if output
+    puts("Page table start: #{page_table}")
+  end
+
+  pages.each_with_index do |new_page, index|
+    # if found do nothing
+    if page_table.include?(new_page)
+      if output
+        puts("Page table: #{page_table} new page: #{new_page} faults: #{faults} HIT")
+      end
+    else
+      # if not found and space available
+      if page_table.length < page_frame_size
+        page_table.push(new_page)
+
+      # if not found and full then replace with distance calcs
+      else
+        search_pages = pages[(index + 1)..-1]
+
+        replace_page = nil
+        furthest_index = -1
+
+        page_table.each do |current_page|
+          current_index = search_pages.index(current_page)
+
+          # infinite case
+          if current_index.nil?
+            replace_page = current_page
+            break
+          # get the furthest so far
+          elsif current_index > furthest_index
+            furthest_index = current_index
+            replace_page = current_page
+          end
+        end
+
+        page_table.delete(replace_page)
+        page_table.push(new_page)
+      end
+
+      faults += 1
+
+      if output
+        puts("Page table: #{page_table} new page: #{new_page} faults: #{faults} MISS")
+      end
+    end
+  end
+
+  return faults
 end
 
 fifo(3, "70120304230321201701", true)
